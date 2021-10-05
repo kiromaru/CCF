9390828bbb8dd495e660fa8f6768ae537f4f54c1
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2021-10-05 23:38:10 +0000

    Serialization seems complete
    Partial-tree serialization is missing though.

--- src/node/history_adapter.h
+++ src/node/history_adapter.h
@@ -11,6 +11,34 @@
 
 #include "oZKS/ozks.h"
 
+namespace {
+    void serialise_u64(std::uint64_t value, std::vector<std::uint8_t>& v)
+    {
+        std::size_t original_size = v.size();
+        v.resize(original_size + sizeof(uint64_t));
+        memcpy(v.data() + original_size, &value, sizeof(uint64_t));
+    }
+
+    void serialise_bytearray(const std::uint8_t* bytes, std::size_t size, std::vector<std::uint8_t>& v)
+    {
+        std::size_t original_size = v.size();
+        v.resize(original_size + size);
+        memcpy(v.data() + original_size, bytes, size);
+    }
+
+    std::uint64_t deserialise_u64(const std::vector<std::uint8_t>& v, std::size_t position)
+    {
+        std::uint64_t result = 0;
+        memcpy(&result, v.data() + position, sizeof(uint64_t));
+        return result;
+    }
+
+    void deserialise_bytearray(std::uint8_t* bytes, std::size_t size, const std::vector<std::uint8_t>& v, std::size_t position)
+    {
+        memcpy(bytes, v.data() + position, size);
+    }
+}
+
 namespace ccf {
     // Adapter to use HistoryTree with OZKS library
     class HistoryTreeAdapter 
@@ -70,6 +98,12 @@ namespace ccf {
             size_t size() const
             {
                 return HASH_SIZE;
+            }
+
+            void to_key(ozks::key_type& key) const
+            {
+                key.resize(HASH_SIZE);
+                memcpy(key.data(), bytes, HASH_SIZE);
             }
         };
 
@@ -145,27 +179,34 @@ namespace ccf {
 
         HistoryTreeAdapter(const std::vector<std::uint8_t>& v)
         {
-
+            deserialise(v);
         }
 
         HistoryTreeAdapter(const Hash& hash)
         {
-
+            insert(hash);
         }
 
-        const Hash& root() const
+        const Hash root() const
         {
-            return root_;
+            return get_root();
         }
 
         std::shared_ptr<Hash> past_root(size_t index) const
         {
-            return std::make_shared<Hash>();
+            if (index >= previous_roots_.size())
+                throw std::runtime_error("Invalid index");
+
+            ozks::commitment_type commitment = previous_roots_[index];
+            return std::make_shared<Hash>(get_root(commitment));
         }
 
-        const Hash& leaf(size_t index) const
+        const Hash leaf(size_t index) const
         {
-            return root_;
+            if (index >= leaves_.size())
+                throw std::runtime_error("Invalid index");
+
+            return leaves_[index];
         }
 
         void flush_to(size_t index)
@@ -185,7 +226,8 @@ namespace ccf {
 
         size_t max_index() const
         {
-            return 0;
+            size_t leaves_ct = leaves_.size();
+            return (leaves_ct == 0) ? 0 : (leaves_ct - 1);
         }
 
         size_t min_index() const
@@ -195,20 +237,93 @@ namespace ccf {
 
         void insert(const Hash& hash)
         {
-            
+            ozks::payload_type empty_payload;
+            ozks::key_type key;
+            hash.to_key(key);
+            tree_.insert(key, empty_payload);
+            leaves_.push_back(hash);
+
+            ozks::Commitment commitment = tree_.get_commitment();
+            previous_roots_.push_back(commitment.root_commitment);
         }
 
         void serialise(std::vector<std::uint8_t>& v) const
         {
+            tree_.save(v);
 
+            // Save leaves vector
+            serialise_u64(leaves_.size(), v);
+            for (std::size_t idx = 0; idx < leaves_.size(); idx++) {
+                serialise_bytearray(leaves_[idx].bytes, Hash::HASH_SIZE, v);
+            }
+
+            // Save past roots vector
+            serialise_u64(previous_roots_.size(), v);
+            for (std::size_t idx = 0; idx < previous_roots_.size(); idx++) {
+                serialise_u64(previous_roots_[idx].size(), v);
+                serialise_bytearray(reinterpret_cast<const std::uint8_t*>(previous_roots_[idx].data()), previous_roots_[idx].size(), v);
+            }
         }
 
-        void serialise(size_t from, size_t to, std::vector<std::uint8_t>& v) const
+        void serialise(std::size_t from, std::size_t to, std::vector<std::uint8_t>& v) const
         {
+            // Ignore from and to for now
+            serialise(v);
+        }
 
+        void deserialise(const std::vector<std::uint8_t>& v)
+        {
+            tree_.clear();
+            leaves_.clear();
+            previous_roots_.clear();
+
+            std::size_t position = tree_.load(v, tree_);
+
+            // Read leaves
+            std::size_t leaves_size = deserialise_u64(v, position);
+            position += sizeof(uint64_t);
+
+            for (std::size_t idx = 0; idx < leaves_size; idx++) {
+                Hash hash;
+                deserialise_bytearray(hash.bytes, Hash::HASH_SIZE, v, position);
+                position += Hash::HASH_SIZE;
+
+                leaves_.push_back(hash);
+            }
+
+            // Read previous roots
+            std::size_t previous_roots_size = deserialise_u64(v, position);
+            position += sizeof(uint64_t);
+
+            for (std::size_t idx = 0; idx < previous_roots_size; idx++) {
+                std::size_t cmt_size = deserialise_u64(v, position);
+                position += sizeof(uint64_t);
+
+                ozks::commitment_type commitment(cmt_size);
+                deserialise_bytearray(reinterpret_cast<uint8_t*>(commitment.data()), cmt_size, v, position);
+                position += cmt_size;
+
+                previous_roots_.push_back(commitment);
+            }
         }
 
     private:
-        Hash root_;
+        ozks::OZKS tree_;
+        std::vector<Hash> leaves_;
+        std::vector<ozks::commitment_type> previous_roots_;
+
+        Hash get_root() const
+        {
+            Hash root;
+            ozks::Commitment commitment = tree_.get_commitment();
+            return get_root(commitment.root_commitment);
+        }
+
+        Hash get_root(const ozks::commitment_type& commitment) const
+        {
+            Hash root;
+            memcpy(root.bytes, commitment.data(), Hash::HASH_SIZE);
+            return root;
+        }
     };
 }
